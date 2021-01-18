b07c11c15dd035829904b134d93c012d470078b3
Alexandra Banica <banica9221@hotmail.be>
2021-01-18 15:57:06 +0100

    chapter one

---
+++ chapter-one/1.3/script.js
@@ -0,0 +1,23 @@
+
+
+
+    let person = prompt("Would you like some sugar free apple pie?");
+    let answer = 'yes';
+
+    if (person === 'yes')
+
+    {
+
+        alert('Congratulations! You have stolen the only food I have for an entire week.... >.<');
+    }
+
+    else
+
+    {
+        alert('Yay, now I can survive this horrible apocalypse. :D Thank you, noble hero');
+    }
+
+
+
+
+
---
+++ chapter-one/1.4/script.js
@@ -0,0 +1,35 @@
+
+(function() {
+
+
+
+
+
+    let age = prompt('What is your age?');
+    let gender = prompt('What your is gender?');
+    let location = prompt('Where do you live?');
+    let validate = confirm("Is this true?" + age + gender + location);
+    let confirmation = prompt('Please, confirm.');
+
+    if (validate === true) {
+
+        alert('Thanks!');
+    } else
+
+    {
+
+        let age = prompt('What is your age?');
+        let gender = prompt('What your is gender?');
+        let location = prompt('Where do you live?');
+    }
+
+
+})();
+
+
+
+
+
+
+
+
