57b39045de3d48e98cfd8201c04dc8cad691b739
Alexandra Banica <banica9221@hotmail.be>
2021-01-26 10:53:35 +0100

    chapter 2

--- chapter-two/2.6/script.js
+++ chapter-two/2.6/script.js
@@ -29,6 +29,12 @@
 
 // multiple of 3 > modulus gebruiken want multiple of = veelvoud
 
+/* zin in html zegt in the console > console.log > for each = for loop > if's > hier if, else if, else if , else
+* getal%15 stond eerst onderaan de function maar deze gaf het daarom niet weer, daarom staat het nu op de eerste plaats
+* zodat het prioteir wordt, op deze manier laat de console elke oplossing zien.
+*
+* */
+
 /*In the console, for each numbers between 1 and 100 : show "fizz" if the number is a multiple of 3,
     "buzz" if it's a multiple of 5, "fizzbuzz" if it's both, or simply the number if it's none of these.
 */
